ddf05462ece59f2f8a37318690aeb826a95d83d5
nitinshukla <nitin.shukla12004@gmail.com>
2025-06-18 15:25:00 +0530

    it is day-01 of learning typescript

---
+++ Day-01/a.js
@@ -0,0 +1,9 @@
+"use strict";
+// const x : number = 5;
+// console.log(x);
+// Task : Write a function that accepts first number as parameter and prints Hello world {firstname} and return nothing.
+function greet(firstname, lastname) {
+    console.log(`Hello world ${firstname} ${lastname}`);
+}
+greet("Nitin", "Shukla");
+greet("Abhay", "Pratap");
---
+++ Day-01/a.ts
@@ -0,0 +1,11 @@
+// const x : number = 5;
+// console.log(x);
+
+// Task : Write a function that accepts first number as parameter and prints Hello world {firstname} and return nothing.
+
+function greet(firstname : string, lastname : string) {
+    console.log(`Hello world ${firstname} ${lastname}`)
+}
+
+greet("Nitin", "Shukla");
+greet("Abhay", "Pratap");
